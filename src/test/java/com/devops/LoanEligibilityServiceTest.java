08e83b7dbf7ab7abe0c881eb5b73e340319823bc
Tharun-coder-hash <tharun26122005@gmail.com>
2026-04-10 21:04:54 +0530

    Create LoanEligibilityServiceTest.java

---
+++ src/test/java/com/devops/LoanEligibilityServiceTest.java
@@ -0,0 +1,31 @@
+package com.devops;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class LoanEligibilityServiceTest {
+
+    @Test
+    void testEligibleUser() {
+        assertEquals("Eligible",
+            LoanEligibilityService.checkEligibility(30, 50000, 700));
+    }
+
+    @Test
+    void testLowAge() {
+        assertEquals("Not Eligible",
+            LoanEligibilityService.checkEligibility(18, 50000, 700));
+    }
+
+    @Test
+    void testLowSalary() {
+        assertEquals("Not Eligible",
+            LoanEligibilityService.checkEligibility(30, 20000, 700));
+    }
+
+    @Test
+    void testLowCreditScore() {
+        assertEquals("Not Eligible",
+            LoanEligibilityService.checkEligibility(30, 50000, 600));
+    }
+}
